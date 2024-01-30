1b678d43001759761973244a1243c63687ac5840
Maraxoni <maraxoni16@gmail.com>
2024-01-30 01:56:37 +0100

    Walidator

--- src/WyswietlQuizy.js
+++ src/WyswietlQuizy.js
@@ -88,31 +88,6 @@ const WyswietlQuizy = ({ listaQuizow, usunQuizZListy }) => {
                     );
                 })}
 
-                {filteredQuizList.map((quiz) => (
-                    <div key={quiz.id} className="quiz-card">
-                        <h3>{quiz.nazwa}</h3>
-                        <p>Kategoria: {quiz.kategoria}</p>
-                        <p>Data dodania: {quiz.dataDodaniaQuizu.toLocaleDateString()}</p>
-                        <p>Data wygaśnięcia: {quiz.dataWygasnieciaQuizu.toLocaleDateString()}</p>
-                        <div>
-                            <Link to={`/rozwiaz-quiz/${quiz.id}`}>
-                                <button>Rozwiąż Quiz</button>
-                            </Link>
-                            {loggedIn && (
-                                <>
-                                    <button onClick={() => handleUsunQuiz(quiz.id)} className="deleteButton">
-                                        Usuń <br />Quiz
-                                    </button>
-                                    <br />
-                                    <Link to={`/edytuj-quiz/${quiz.id}`}>
-                                        <button className="editButton">Edytuj<br /> Quiz</button>
-                                    </Link>
-                                </>
-                            )}
-                        </div>
-                    </div>
-                ))}
-
             </div>
             {rozwiazanieQuizu && <RozwiazQuiz quizId={rozwiazanieQuizu} onClose={handleCloseRozwiazQuiz} />}
         </div>
